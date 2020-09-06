16d3378bb3d01d29258e1c376d49dff43df9981a
Haylee Ham <hayleeham@gmail.com>
2020-09-05 20:53:36 -0400

    finish xml_parse

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # burning_glass_database
+## Pulling out employers from all files
+`python3 xml_parse.py`
---
+++ xml_parse.py
@@ -0,0 +1,19 @@
+import zipfile
+import lxml.etree as ET
+import os
+
+path = '/export/projects2/rsadun_burning_glass_project/Burning Glass Data/Text Data/'
+years = ['2007', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', 
+         '2018', '2018_ongoing', '2019', '2020']
+
+with open('outfile', 'wt') as out:
+    for year in years:
+        print(year)
+        for filename in os.listdir(path + year):
+            print(filename)
+            zf = zipfile.ZipFile(filename)
+            xml_filename = filename[:-3] + 'xml'
+            for event, elem in ET.iterparse(zf.open(xml_filename), events=("end",)):
+                if elem.tag == 'CanonEmployer':
+                    out.write(f'{elem.text}\n')
+                    elem.clear()
